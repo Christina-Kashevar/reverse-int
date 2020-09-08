2bf3006b9f8bdedd407b4ff922b41f07aed992ac
Christina Kashevar <christi_k@mail.ru>
2020-09-08 20:44:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  if (n >=0) {
+    return n = +n.toString().split('').reverse().join('');
+  }
+  if (n < 0){
+    return n = +n.toString().split('').slice(1).reverse().join('');
+  }
 }
